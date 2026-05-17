1080df912a3e40252342e30dfbd973eae5bc8b40
Dzheyda <Dzheyda@DESKTOP-EK7Q6VR>
2026-05-17 23:18:26 +0300

    SqlAttendanceRepository, SqlStudentRepository

--- Infrastructure/SqlRepositories/SqlAttendanceRepository.cs
+++ Infrastructure/SqlRepositories/SqlAttendanceRepository.cs
@@ -9,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace SchoolManagementSystem.Infrastructure.SqlRepositories
 {
-    public class SqlAttendanceRepository
+    public class SqlAttendanceRepository : IAttendanceRepository
     {
+        private readonly SchoolDbContext _db;
+
+        public SqlAttendanceRepository(SchoolDbContext db)
+        {
+            _db = db;
+        }
+
+        public IReadOnlyList<Attendance> GetAll()
+        {
+            return _db.Attendances
+                .Include(a => a.Student)
+                .ToList();
+        }
+
+        public Attendance GetById(int id)
+        {
+            return _db.Attendances
+                .Include(a => a.Student)
+                .FirstOrDefault(a => a.Id == id);
+        }
+        public void Save(Attendance attendance)
+        {
+            if (attendance.Id == 0)
+            {
+                _db.Attendances.Add(attendance);
+            }
+            else
+            {
+                _db.Attendances.Update(attendance);
+            }
+            _db.SaveChanges();
+        }
     }
 }
--- Infrastructure/SqlRepositories/SqlStudentRepository.cs
+++ Infrastructure/SqlRepositories/SqlStudentRepository.cs
@@ -1,13 +1,60 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolManagementSystem.Application.Interfaces;
+using SchoolManagementSystem.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SchoolManagementSystem.Application.Interfaces;
 
 namespace SchoolManagementSystem.Infrastructure.SqlRepositories
 {
-    public class SqlStudentRepository
+    public class SqlStudentRepository : IStudentRepository  
     {
+
+        private readonly SchoolDbContext _db;
+
+        public SqlStudentRepository(SchoolDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IReadOnlyList<Student> GetAll()
+        {
+            return _db.Students
+                .Include(s => s.Class)
+                .Include(s => s.grades)
+                .Include(s => s.attendances)
+                .ToList();
+        }
+
+        public Student GetById(int id)
+        {
+            return _db.Students
+                .Include(s => s.Class)
+                .Include(s => s.grades)
+                .Include(s => s.attendances)
+                .FirstOrDefault(s => s.Id == id);
+        }
+
+        public void Save(Student student)
+        {
+            if (student.Id == 0)
+            {
+                _db.Students.Add(student);
+            }
+            else
+            {
+                _db.Students.Update(student);
+            }
+
+            _db.SaveChanges();
+        }
     }
+
+
+
+
 }
+
